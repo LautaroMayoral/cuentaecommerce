35a49cb7311226ca7d31ce355cb3151d183ec7ad
LautaroMayoral <166188846+LautaroMayoral@users.noreply.github.com>
2025-03-20 11:37:40 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Servidor de Ecommerce está funcionando!');
+});
+
+app.listen(5000, () => {
+    console.log('Servidor en el puerto 5000');
+});
 
