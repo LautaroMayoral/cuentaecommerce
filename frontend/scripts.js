462048308bef453a1583130d921c287efcd4aa74
LautaroMayoral <166188846+LautaroMayoral@users.noreply.github.com>
2025-03-20 13:05:05 -0300

    Create scripts.js

---
+++ frontend/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Actualiza la cantidad de productos en el carrito
+    let cantidadProductos = 0; // Esta variable debe actualizarse según la lógica de tu carrito
+    document.getElementById('cantidad-productos').textContent = cantidadProductos;
+});
