9c0a81d133381a31cabf4d19cea7ce238fb4fede
LautaroMayoral <166188846+LautaroMayoral@users.noreply.github.com>
2025-03-21 11:53:41 -0300

    Update webcontent.js

--- frontend/webcontent.js
+++ frontend/webcontent.js
@@ -1,5 +1,17 @@
+// Definición de browserObject
+var browserObject = {
+    name: 'Chrome',
+    version: '89.0',
+    isMobile: false,
+    openNewTab: function(url) {
+        window.open(url, '_blank');
+    }
+};
+
+// Uso de browserObject
 if (typeof browserObject !== 'undefined') {
-    // Código que utiliza browserObject
+    console.log('Browser Name:', browserObject.name);
+    browserObject.openNewTab('https://www.mayoralimp.com');
 } else {
     console.error('browserObject is undefined');
 }
