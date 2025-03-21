a62eab015b1e945590603c4905702ee792e73976
LautaroMayoral <166188846+LautaroMayoral@users.noreply.github.com>
2025-03-21 12:21:57 -0300

    Update webcontent.js

--- frontend/webcontent.js
+++ frontend/webcontent.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log('Web content loaded successfully.');
     
     // Función para abrir una nueva pestaña
-    function openNewTab(url) {
-        window.open(url, '_blank');
+    //function openNewTab(url) {
+      //  window.open(url, '_blank');
     }
 
     // Ejemplo de uso (comentado para evitar que se ejecute automáticamente)
